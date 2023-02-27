ceb64447a85a8e83f7ab918c4b394a4760cd9206
Maciej Kudej <maciejkudej765@gmail.com>
2023-02-27 20:41:44 +0100

    Final version task7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+createGalleryItems();
+
+function createGalleryItems() {
+  const items = [];
+  for (let i = 0; i < galleryItems.length; i++) {
+    const galleryItem = document.createElement("a");
+    galleryItem.classList.add("gallery__item");
+    galleryItem.href = galleryItems[i].original;
+    galleryItem.appendChild(createGalleryImage(galleryItems[i]));
+    items.push(galleryItem);
+  }
+  gallery.append(...items);
+}
+
+function createGalleryImage(galleryItemFromList) {
+  let img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = galleryItemFromList.preview;
+  img.alt = galleryItemFromList.description;
+  return img;
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
